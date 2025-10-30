9c36a6a04f7be16dd7c6e6a3802add2c64cd7a88
Z-Moore548 <zmoore1@nait.ca>
2025-10-30 13:58:16 -0600

    updated it to look better

--- ShootingThings/Assets/Scrips/GameTracker.cs
+++ ShootingThings/Assets/Scrips/GameTracker.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class GameTracker : MonoBehaviour
 {
     [SerializeField] bool isQuestActive, gotMap, tresureGot;
-    [SerializeField] GameObject tresure;
+    [SerializeField] GameObject tresure, UI;
 
     public bool IsQuestActive { get => isQuestActive; set => isQuestActive = value; }
     public bool GotMap { get => gotMap; set => gotMap = value; }
@@ -28,6 +28,7 @@ public class GameTracker : MonoBehaviour
         {
             if (tresureGot)
             {
+                UI.GetComponent<QuestAndUI>().IsInteractable(false, 3);
                 tresure.SetActive(false);
             }
             else
